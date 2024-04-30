6cdfd9e84421541586c5c0f67a650ef36d47a79b
abhishekpuranipd <abhishekpuranikpd@gmail.com>
2024-04-30 18:01:04 +0530

    #footer bug

--- src/components/ui/footer.jsx
+++ src/components/ui/footer.jsx
@@ -7,11 +7,11 @@ const FooterBar = () => {
 
 
   return (
-    <footer className="bg-slate-800 text-white font-sans font-semibold py-4 p-10">
+    <footer className="bg-slate-800 text-white font-sans font-semibold py-4 p-10 bottom-0">
             <div className="container mx-auto">
                 <div className="justify-between items-center">
                     <div>
-                        <p className="text-sm">&copy; {new Date().getFullYear()} Nyaya Kutira. All rights reserved.</p>
+                        <p className="text-sm">&copy; {new Date().getFullYear()} Kavitva. All rights reserved.</p>
                         <p className="text-sm">Developed By Shivendu Infotech Pvt Ltd</p>
                     </div>
                    
